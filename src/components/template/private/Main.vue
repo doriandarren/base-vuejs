b959d7c4f5c1e61967dde7e56f554347983072dc
Dorian Gonzalez <doriandarren1@gmail.com>
2024-05-28 11:19:34 +0200

    rm transition menu

--- src/components/template/private/Main.vue
+++ src/components/template/private/Main.vue
@@ -3,20 +3,14 @@
     <!-- Mobile Sidebar -->
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog as="div" class="relative z-50 lg:hidden" @close="sidebarOpen = false">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
-          enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
-          leave-to="opacity-0">
+        <TransitionChild as="template">
           <div class="fixed inset-0 bg-gray-900/80" />
         </TransitionChild>
 
         <div class="fixed inset-0 flex">
-          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
-            enter-from="-translate-x-full" enter-to="translate-x-0"
-            leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
-            leave-to="-translate-x-full">
+          <TransitionChild as="template">
             <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
-              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
-                enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+              <TransitionChild as="template">
                 <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
                   <button type="button" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
                     <span class="sr-only">Close sidebar</span>
@@ -27,8 +21,7 @@
               <!-- Sidebar component, swap this element with another sidebar if you like -->
               <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
                 <div class="flex h-16 shrink-0 items-center">
-                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company" />
+                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
                 </div>
 
                 <nav class="flex flex-1 flex-col">
@@ -37,7 +30,7 @@
                       <ul role="list" class="-mx-2 space-y-1">
                         <li v-for="item in navigation" :key="item.name">
                           <router-link v-if="!item.children" :to="{ name: item.href }" @click="closeSidebar"
-                            :class="[currentRoute === item.href ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-gray-700']">
+                            :class="[currentRoute === item.href ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-gray-300']">
                             <component :is="item.icon" class="h-6 w-6 shrink-0 text-gray-400" aria-hidden="true" />
                             {{ item.name }}
                           </router-link>
@@ -51,17 +44,11 @@
                                 :class="[isDropdownOpen(item.name) ? 'rotate-90 text-gray-500' : 'text-gray-400', 'ml-auto h-5 w-5 shrink-0']"
                                 aria-hidden="true" />
                             </button>
-                            <transition
-                              enter-active-class="transition-opacity transition-transform ease-in-out duration-300"
-                              enter-from-class="opacity-0 transform -translate-y-2"
-                              enter-to-class="opacity-100 transform translate-y-0"
-                              leave-active-class="transition-opacity transition-transform ease-in-out duration-300"
-                              leave-from-class="opacity-100 transform translate-y-0"
-                              leave-to-class="opacity-0 transform -translate-y-2">
+                            <transition>
                               <ul v-if="isDropdownOpen(item.name)" class="mt-1 px-2">
                                 <li v-for="subItem in item.children" :key="subItem.name">
                                   <router-link :to="{ name: subItem.href }" @click="closeSidebar"
-                                    :class="[currentRoute === subItem.href ? 'bg-gray-800 text-white' : 'hover:bg-gray-50', 'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700']">
+                                    :class="[currentRoute === subItem.href ? 'bg-gray-800 text-white' : 'hover:bg-gray-50', 'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-300']">
                                     {{ subItem.name }}
                                   </router-link>
                                 </li>
@@ -104,7 +91,7 @@
               <ul role="list" class="-mx-2 space-y-1">
                 <li v-for="item in navigation" :key="item.name">
                   <router-link v-if="!item.children" :to="{ name: item.href }" @click="closeSidebar"
-                    :class="[currentRoute === item.href ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-gray-700']">
+                    :class="[currentRoute === item.href ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-gray-300']">
                     <component :is="item.icon" class="h-6 w-6 shrink-0 text-gray-400" aria-hidden="true" />
                     {{ item.name }}
                   </router-link>
@@ -118,17 +105,11 @@
                         :class="[isDropdownOpen(item.name) ? 'rotate-90 text-gray-500' : 'text-gray-400', 'ml-auto h-5 w-5 shrink-0']"
                         aria-hidden="true" />
                     </button>
-                    <transition
-                      enter-active-class="transition-opacity transition-transform ease-in-out duration-300"
-                      enter-from-class="opacity-0 transform -translate-y-2"
-                      enter-to-class="opacity-100 transform translate-y-0"
-                      leave-active-class="transition-opacity transition-transform ease-in-out duration-300"
-                      leave-from-class="opacity-100 transform translate-y-0"
-                      leave-to-class="opacity-0 transform -translate-y-2">
+                    <transition>
                       <ul v-if="isDropdownOpen(item.name)" class="mt-1 px-2">
                         <li v-for="subItem in item.children" :key="subItem.name">
                           <router-link :to="{ name: subItem.href }" @click="closeSidebar"
-                            :class="[currentRoute === subItem.href ? 'bg-gray-800 text-white' : 'hover:bg-gray-50', 'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-700']">
+                            :class="[currentRoute === subItem.href ? 'bg-gray-800 text-white' : 'hover:bg-gray-50', 'block rounded-md py-2 pr-2 pl-9 text-sm leading-6 text-gray-300']">
                             {{ subItem.name }}
                           </router-link>
                         </li>
@@ -179,7 +160,7 @@
                 </span>
               </MenuButton>
 
-              <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+              <transition>
                 <MenuItems class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                   <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
                     <a :href="item.href" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']" @click="item.onClick">
