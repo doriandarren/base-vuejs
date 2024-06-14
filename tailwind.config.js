e42c5e291ad7a5e19cbebc6fcb2d14e8bdf37e4f
Dorian Gonzalez <doriandarren1@gmail.com>
2024-06-14 16:35:07 +0200

    add main js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ export default {
       fontFamily: {
         'sans': ['Lato', 'sans-serif'], // Usar Lato como fuente sans
       },
+      colors: {
+        primary: '#0096b2', // cian
+        'primary-dark': '#007a91', // cian oscuro
+        'primary-light': '#00b4d6', // cian claro
+        secondary: '#ff6347', // tomate
+        'secondary-dark': '#cc4f3a', // tomate oscuro
+        'secondary-light': '#ff7f6e', // tomate claro
+        accent: '#ffc107', // ámbar
+        'accent-dark': '#cc9a06', // ámbar oscuro
+        'accent-light': '#ffca28', // ámbar claro
+        neutral: '#f5f5f5', // gris claro
+        'neutral-dark': '#e0e0e0', // gris
+        'neutral-light': '#fafafa', // gris más claro
+      },
     },
   },
   plugins: [],
