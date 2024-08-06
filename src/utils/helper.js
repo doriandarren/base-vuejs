ab5268f5e6ab44565d7f218a28491360a5f134ba
Dorian Gonzalez <doriandarren1@gmail.com>
2024-08-06 19:04:26 +0200

    add formatMoney helper

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,5 +1,4 @@
-
-export function randomNumbers(from, to, length) {
+function randomNumbers(from, to, length) {
     const numbers = [0];
     for (let i = 1; i < length; i++) {
     numbers.push(Math.ceil(Math.random() * (from - to) + to));
@@ -8,8 +7,27 @@ export function randomNumbers(from, to, length) {
 }
 
 
-export function delay(ms) {
+function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
+
+/**
+ * Return money format
+ * EX. 1340.00 -> 1.340,00 €
+ */
+const formatMoney = (valor) => {
+    const formatter = new Intl.NumberFormat('es-ES', {
+        style: 'currency',
+        currency: 'EUR'
+    })
+    return formatter.format(valor);
+};
+
+
+export{
+    randomNumbers,
+    delay,
+    formatMoney
+}
