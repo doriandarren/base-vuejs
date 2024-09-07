b9210016c7b3c11fa80e680d1c7d108f40b8a6db
Dorian Gonzalez <doriandarren1@gmail.com>
2024-09-07 22:08:35 +0200

    add route

--- src/components/template/public/Main.vue
+++ src/components/template/public/Main.vue
@@ -4,18 +4,39 @@
         <main class="flex-grow">
             <router-view />
         </main>
-        <Footer />
+        <Footer ref="footer" />
     </div>
 </template>
 
 <script setup>
 
-import { RouterView } from 'vue-router';
+import { watch, ref } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
 import Footer from '@/components/template/public/Footer.vue';
 import TopBar from '@/components/template/public/TopBar.vue';
 
+
+// Observamos el cambio de ruta
+const route = useRoute();
+const footer = ref(null);
+
+watch(route, () => {
+  // Reiniciamos la animación del footer al cambiar de ruta
+  const footerElement = footer.value.$el || footer.value; 
+  footerElement.classList.remove('animate__animated', 'animate__fadeInUp', 'animate__fast');
+  void footerElement.offsetWidth; // Forzamos el reflow para reiniciar la animación
+  footerElement.classList.add('animate__animated', 'animate__fadeInUp', 'animate__fast');
+});
+
+
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+/* Estilos de transición para las rutas */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
